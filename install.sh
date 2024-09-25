4189f8177ce5dff00119b6ff856f8358acde4c80
Yani Purlina <yanipurlina@gmail.com>
2024-09-25 07:15:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ install_theme() {
     read -r SELECT_THEME
     case "$SELECT_THEME" in
       1)
-        THEME_URL=$(echo -e "https://github.com/Farisofc/panel/raw/main/C2.zip")
+        THEME_URL=$(echo -e "https://github.com/yanipurlina7/BashFree/raw/main/C2.zip")
         break
         ;;
       2)
@@ -97,7 +97,7 @@ install_theme() {
         break
         ;;
       3)
-        THEME_URL=$(echo -e "https://github.com/Farisofc/panel/raw/main/C3.zip")
+        THEME_URL=$(echo -e "https://github.com/yanipurlina7/BashFree/raw/main/C3.zip")
         break
         ;; 
       x)
